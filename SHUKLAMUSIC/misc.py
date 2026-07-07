015093219c6ea4647e0969d22e49be2f26c92a7c
suspiciousboy-bots <suspiciousboy1412@gmail.com>
2026-07-07 15:54:59 +0800

    Update misc.py

--- SHUKLAMUSIC/misc.py
+++ SHUKLAMUSIC/misc.py
@@ -1,49 +1,12 @@
-# -----------------------------------------------
-# 🔸 StrangerMusic Project
-# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
-# 📅 Copyright © 2022 – All Rights Reserved
-#
-# 📖 License:
-# This source code is open for educational and non-commercial use ONLY.
-# You are required to retain this credit in all copies or substantial portions of this file.
-# Commercial use, redistribution, or removal of this notice is strictly prohibited
-# without prior written permission from the author.
-#
-# ❤️ Made with dedication and love by ItzShukla
-# -----------------------------------------------
-import socket
+# SHUKLAMUSIC/misc.py
 import time
-import heroku3
 from pyrogram import filters
 import config
 from SHUKLAMUSIC.core.mongo import mongodb
 from .logging import LOGGER
 
 SUDOERS = filters.user()
-
-HAPP = None
 _boot_ = time.time()
-
-
-def is_heroku():
-    return "heroku" in socket.getfqdn()
-
-
-XCB = [
-    "/",
-    "@",
-    ".",
-    "com",
-    ":",
-    "git",
-    "heroku",
-    "push",
-    str(config.HEROKU_API_KEY),
-    "https",
-    str(config.HEROKU_APP_NAME),
-    "HEAD",
-    "master",
-]
 
 
 def dbb():
@@ -72,14 +35,6 @@ async def sudo():
 
 
 def heroku():
-    global HAPP
-    if is_heroku:
-        if config.HEROKU_API_KEY and config.HEROKU_APP_NAME:
-            try:
-                Heroku = heroku3.from_key(config.HEROKU_API_KEY)
-                HAPP = Heroku.app(config.HEROKU_APP_NAME)
-                LOGGER(__name__).info(f"🍟𝗛𝗘𝗥𝗢𝗞𝗨 𝗔𝗣𝗣 𝗡𝗔𝗠𝗘 𝗟𝗢𝗔𝗗......💦")
-            except BaseException:
-                LOGGER(__name__).warning(
-                    f"✨𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝 𝐇𝐞𝐫𝐨𝐤𝐮 𝐀𝐩𝐢 𝐊𝐞𝐲 𝐀𝐧𝐝 𝐇𝐞𝐫𝐨𝐤𝐮 𝐀𝐩𝐩 𝐍𝐚𝐦𝐞 🕊️𝐂𝐨𝐫𝐫𝐞𝐜𝐭...."
-)
+    # No Heroku needed for Railway
+    LOGGER(__name__).info("📦 Running on Railway - Heroku features disabled")
+    return None
